1102558079d8e50f519bb2cce73fb07933cba436
Danish11150 <danishmughalkids@gmail.com>
2026-05-26 03:13:22 +0300

    Change app.run to use environment variable for port
    Update app.run to use dynamic port assignment from environment variable.

--- main.py
+++ main.py
@@ -52,5 +52,7 @@ def whatsapp_bot():
     return jsonify({"replies": [{"message": ai_reply}]})
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    # Render khud port assign karta hai, agar na mile toh 5000 use karega
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
   
